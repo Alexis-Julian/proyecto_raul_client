6601f702e759603de0f596d5b7581ebb37eaa78e
Alexis <alexisjrojas@hotmail.es>
2023-09-18 08:57:42 -0300

    Login preparado para enviar usuarios

--- src/layouts/RenderLogin.tsx
+++ src/layouts/RenderLogin.tsx
@@ -1,24 +1,41 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 interface PropsLogin {
 	html: XMLDocument;
 }
 
 function RenderLogin({ html }: PropsLogin) {
-	function handeFormSubmit(e: SubmitEvent): any {
-		e.preventDefault();
-	}
-
 	/* UseEffect para recuperar los datos del html */
+
 	useEffect(() => {
 		if (html) {
 			const form = document.getElementById("form");
+
 			if (form) {
-				form.addEventListener("submit", (e) => handeFormSubmit(e));
+				form.addEventListener("submit", (e: any) => handeFormSubmit(e));
 			}
 		}
 	});
+
+	async function handeFormSubmit(e: React.ChangeEvent<HTMLFormElement>) {
+		e.preventDefault();
+		const svg_charge = document.getElementById("charge");
+
+		if (e.target) {
+			/* Payload para el backend */
+			const user = {
+				email: e.target["email"].value,
+				password: e.target["password"].value,
+			};
+
+			svg_charge?.classList.add("animate-spin");
+
+			await new Promise((resolve) => setTimeout(resolve, 6000));
+
+			svg_charge?.classList.remove("animate-spin");
+		}
+	}
 
 	return (
 		<main className="h-full" dangerouslySetInnerHTML={{ __html: html }}></main>
